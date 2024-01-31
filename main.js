df97ef811107a0a1d503c1fdac317ba045f13f57
Zycebx <142185913+Zycebx@users.noreply.github.com>
2024-01-30 19:38:36 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -14,20 +14,27 @@ var Content = event.results[0][0].transcript;
 console.log(Content);
 
 document.getElementById("textbox").innerHTML = Content;
-
-speak();
+if(Content =="Take my selfie." || Content =="take my selfie")
+{
+    speak();
+}
 }
 
 function speak()
 {
     var synth = window.speechSynthesis
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your selfie in 5 seconds, smile";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function(){
+        take_snapshot();
+        svae();
+    },5000);
 }
 
 Webcam.set({
@@ -37,3 +44,25 @@ Webcam.set({
     png_quality:90
 });
 camera=document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function svae()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.href = image;
+    link.click();
+}
+
+
+
+
+
+
+
